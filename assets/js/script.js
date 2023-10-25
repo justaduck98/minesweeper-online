4a9689905de54ac2f4dafccb5ec54dc87d32604c
justaduck98 <>
2023-10-25 09:45:30 +0000

    Add removal of old playing fields if there was one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,37 @@
 //Default game is 5x5 which is generated once the page is loaded
-window.onload = function(){
+window.onload = function () {
     runGame(5);
-}
-    
+};
 
-document.getElementById('option1').addEventListener("click", function(){
+
+document.getElementById('option1').addEventListener("click", function () {
     /*Call run game function as well as generate bombs with parameter of number of squares and to generate*/
     runGame(5);
 });
 
-document.getElementById('option2').addEventListener("click", function(){
-   runGame(7);
+document.getElementById('option2').addEventListener("click", function () {
+    runGame(7);
 });
 
-document.getElementById('option3').addEventListener("click", function(){
+document.getElementById('option3').addEventListener("click", function () {
     runGame(9);
 });
 
-function generateBombs(gridSize){
-        let numberOfBombs = Math.floor(Math.random()* gridSize) + 1;
-        document.getElementById("bombs").innerHTML = "Number of Bombs: " + numberOfBombs;
-        console.log(numberOfBombs);
+function generateBombs(gridSize) {
+    let numberOfBombs = Math.floor(Math.random() * gridSize) + 1;
+    document.getElementById("bombs").innerHTML = "Number of Bombs: " + numberOfBombs;
+    console.log(numberOfBombs);
 }
 
-function generatePlayingField(gridSize){
+function generatePlayingField(gridSize) {
     let playingField = document.createElement('div');
-    for(let x = 1; x <=gridSize; x++){
+    for (let x = 1; x <= gridSize; x++) {
         //Creates a row container for the following 5 elements
         let tileRow = document.createElement('div');
         tileRow.classList.add('row');
-        for (let y= 1; y <= gridSize; y++){
+        for (let y = 1; y <= gridSize; y++) {
             // generates id's in a coordinate system to allow checkBombFunction
-            id= `${x},${y}`;
+            id = `${x},${y}`;
             let tile = document.createElement('p');
             tile.setAttribute('id', id);
             tile.textContent = 'X';
@@ -42,20 +42,22 @@ function generatePlayingField(gridSize){
         playingField.appendChild(tileRow);
     }
     //removes old playing field if there is one
-    /*let oldField = document.getElementById('gameArea');
-    if(oldField){
-        document.getElementById('gameArea').removeChild(playingField);
-    }*/
+    let oldField = document.getElementById('gameArea');
+    if (oldField.hasChildNodes()) {
+        oldField.removeChild(oldField.firstChild);
+    }
     //adds playing field to gameArea div
     document.getElementById('gameArea').appendChild(playingField);
+
+
 }
 
-function runGame(difficulty){
+function runGame(difficulty) {
     generateBombs(difficulty);
     let game = generatePlayingField(difficulty);
     console.log(game);
 }
 
-function clickTile(event){
+function clickTile(event) {
     console.log('I was clicked');
 }
