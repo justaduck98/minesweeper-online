1b32697776dca55d1127a66b19669117c2f282c2
justaduck98 <>
2023-10-26 01:12:14 +0000

    Add functionality to count bombs around clicked tiles, and set text content to the amount of bombs that were nearby

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,8 @@ document.getElementById('option3').addEventListener("click", function () {
     runGame(9);
 });
 
-document.getElementById('flag').addEventListener('change', function(){
-    if(flag.checked){
+document.getElementById('flag').addEventListener('change', function () {
+    if (flag.checked) {
         flagToggled = true;
     }
     else {
@@ -44,7 +44,7 @@ function generatePlayingField(gridSize) {
             let tile = document.createElement('span');
             tile.setAttribute('id', id);
             tile.textContent = '';
-            tile.addEventListener('click', clickTile); 
+            tile.addEventListener('click', clickTile);
             tile.addEventListener('right-click', clickTile);
             tileRow.appendChild(tile);
             console.log(id);
@@ -66,24 +66,22 @@ function generateBombs(gridSize) {
 
     let numberOfBombs = Math.floor(Math.random() * gridSize) + 3;
     let bombPositions = [];
-    for (let i = 0; i< numberOfBombs;i++){
+    for (let i = 0; i < numberOfBombs; i++) {
         //generate random coordinates
         let xCoord = Math.floor(Math.random() * gridSize) + 1;
-        
+
         let yCoord = Math.floor(Math.random() * gridSize) + 1;
         //gets the tile.id to insert class="bomb"
         let bomb = document.getElementById(`${xCoord},${yCoord}`);
         //checks if tile.id already has class="bomb"
-        if(bomb.classList.contains(bomb)){
-            i--; //makes sure there's not too few bombs by making the loop run again
+        if (bomb.classList.contains(bomb)) {
+            i--; //makes sure there's no duplicate bombs by making the loop run again
         }
-        else{
+        else {
             bomb.classList.add("bomb");
         }
     }
     document.getElementById("bombs").innerHTML = `Number of bombs: ${numberOfBombs}`;
-    console.log(bombPositions);
-    return bombPositions;
 }
 /**A function to run all the necessary functions to play the game */
 function runGame(difficulty) {
@@ -95,38 +93,78 @@ function runGame(difficulty) {
 /**A handler for clicking on a tile on the website */
 function clickTile(event) {
     let tile = event.target;
-    if(flagToggled == true){
+    if (flagToggled == true) {
         console.log("flagToggled was true");
         setFlag(tile);
     }
-    else{
+    else {
         checkBombPosition(tile);
     }
     //adding .id after tile shows the id of the paragraph clicked, instead of outputting "HTMLParagraphElement"
     console.log(`The tile clicked was: ${tile.id}`);
 }
 /**Adds flag placement functionality */
-function setFlag(tile){
-    if(tile.innerHTML.includes('fa-flag')){
-        tile.innerHTML= '';
+function setFlag(tile) {
+    if (tile.innerHTML.includes('fa-flag')) {
+        tile.innerHTML = '';
     }
-    else{
+    else {
         tile.innerHTML = `<i class="fa-solid fa-flag"id="flag"></i>`;
     }
 
 }
 
 /**Checks if tile clicked by user contains bomb */
-function checkBombPosition(tile){
+function checkBombPosition(tile) {
     let numberOfBombs = document.getElementById("bombs").textContent[17];
-    for(let i = 0; i <numberOfBombs; i++){
-        if(tile.classList.contains('bomb')){
+    for (let i = 0; i < numberOfBombs; i++) {
+        if (tile.classList.contains('bomb')) {
             let bombTiles = document.getElementsByClassName('bomb');
             bombTiles[i].innerHTML = `<i class="fa-solid fa-bomb"></i>`;
             bombTiles[i].style.color = "black";
             document.getElementById('bombs').innerHTML = "BOOM! You found a bomb! GAME OVER!!";
             document.getElementsByTagName('h2')[1].innerHTML = "Would You Like To Play Again?";
         }
+        else {
+            revealTile(tile);
+        }
     }
+
+}
+/**This function will check nearby tiles for bombs and reveal them if there are no bombs */
+function revealTile(tile) {
+    let nearbyBombs = countBombs(tile);
+    tile.classList.add(`b${nearbyBombs}`);
+    tile.textContent = nearbyBombs;
+    if (nearbyBombs === 0) {
+        document.getElementById
+    }
+    tile.style.backgroundColor = "lightgrey";
+}
+
+function countBombs(tile){
+    let coords = tile.id;
+    let x = parseInt(coords[0]);
+    let y = parseInt(coords[2]);
+    let bombCount = 0;
+    // adds coordinates around the tile to check for bombs
+    let adjecentTiles =[
+        [x, y+1],
+        [x+1, y+1],
+        [x-1, y],
+        [x+1,y],
+        [x-1, y-1],
+        [x, y-1],
+        [x+1, y-1],
+    ];
+    /**Loops through the nested array above of x and y coordinates adjacent to the clicked tile */
+    for (let [adjecentX, adjecentY] of adjecentTiles) {
+        let adjecentTile = document.getElementById(`${adjecentX},${adjecentY}`);
+        if(adjecentTile && adjecentTile.classList.contains('bomb')){
+            bombCount += 1;
+        }        
+        
+    }
+    return bombCount;
     
 }
